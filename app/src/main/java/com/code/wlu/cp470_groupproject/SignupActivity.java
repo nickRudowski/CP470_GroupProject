0287ea2e3250596071b58f98fac718a39624e014
Hamzah <ha,zah.raza99@gmail.com>
2022-11-29 17:45:28 -0500

    added default settings to user in database (name, notification pref) on sign up

--- app/src/main/java/com/code/wlu/cp470_groupproject/SignupActivity.java
+++ app/src/main/java/com/code/wlu/cp470_groupproject/SignupActivity.java
@@ -17,11 +17,19 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class SignupActivity extends AppCompatActivity {
 
     protected static final String ACTIVITY_NAME = "SignupActivity";
 
     private FirebaseAuth mAuth;
+    public FirebaseDatabase database = FirebaseDatabase.getInstance();
+    public DatabaseReference DatabaseRef = database.getReference("Users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +41,20 @@ public class SignupActivity extends AppCompatActivity {
         Button register = findViewById(R.id.signup_button);
         TextView edit_email = findViewById(R.id.edit_register_email);
         TextView edit_password = findViewById(R.id.edit_register_password);
+        TextView edit_name = findViewById(R.id.edit_register_name);
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email_string = edit_email.getText().toString();
                 String password_string = edit_password.getText().toString();
-                createAccount(email_string, password_string);
+                String name = edit_name.getText().toString();
+                createAccount(email_string, password_string, name);
             }
         });
     }
 
-    private void createAccount(String email, String password) {
+    private void createAccount(String email, String password, String name) {
 //        if (!validateForm()) {
 //            return;
 //        }
@@ -56,7 +66,9 @@ public class SignupActivity extends AppCompatActivity {
                             if (task.isSuccessful()) {
                                 // Sign in success, update UI with the signed-in user's information
                                 FirebaseUser user = mAuth.getCurrentUser();
-                                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                                DatabaseRef.child(email.replaceAll("[.]", "")).child("UserDetails").child("Name").setValue(name);
+                                DatabaseRef.child(email.replaceAll("[.]", "")).child("UserDetails").child("Notification Pref").setValue("2");
+                                Intent intent = new Intent(SignupActivity.this, SubscriptionList.class);
                                 intent.putExtra("UserName", email);
                                 startActivity(intent);
 //                    updateUI(user);
